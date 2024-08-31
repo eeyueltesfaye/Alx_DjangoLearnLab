ebc82a7fae89fb0ff2f67607253a5488a6f6d32b
eeyueltesfaye <eyueltesfaye6@gmail.com>
2024-08-31 21:00:36 +0300

    updated api_project

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,12 +2,9 @@ from django.shortcuts import render
 from rest_framework import generics 
 from .models import Book 
 from .serializers import BookSerializer
-from rest_framework import viewsets
 
-
-class BookViewSet(viewsets.ModelViewSet):
+class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer 
-
 
 
