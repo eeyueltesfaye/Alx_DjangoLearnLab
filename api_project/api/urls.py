dfde2873f5794e471c34e0bce28dc60061b79889
eeyueltesfaye <eyueltesfaye6@gmail.com>
2024-08-31 21:10:42 +0300

    added crud operations in the api_project

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,17 @@
-from django.urls import path
-from .views import BookList
+from django.urls import path, include 
+from .views import BookList, BookViewSet
+from rest_framework.routers import DefaultRouter
 
+
+# Existing URL pattern for BookList view
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
+]
+
+# New router for BookViewSet
+router = DefaultRouter()
+router.register(r'books-crud', BookViewSet)  # Use a different prefix to avoid conflict
+
+urlpatterns += [
+    path('', include(router.urls)),
 ]
