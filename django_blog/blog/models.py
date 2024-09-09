81eddeb203ac16433be821d0ffdb08e73bd19bb4
eeyueltesfaye <eyueltesfaye6@gmail.com>
2024-09-09 18:59:47 +0300

    Initial Setup and Project Configuration for a Django Blog

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -4,9 +4,6 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    publication_date = models.DateTimeField(auto_now_add=True)
+    published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
 
-
-    def __str__(self):
-        return self.title  
--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-5c0*1i_!la7ofq3lqvusn)4!2p24_pcg%26yd)vz2s#5#-as@r"
+SECRET_KEY = "django-insecure-trq9up=ik#__x(ek82w)$9p+s(y-dp%5*i2&k6j5(1j(!!nz@b"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -122,8 +122,6 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / 'blog/static']
-
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
