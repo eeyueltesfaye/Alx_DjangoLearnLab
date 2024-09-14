8bee1f732c260d40d1c060bd69714eeff4d4a44c
eeyueltesfaye <eyueltesfaye6@gmail.com>
2024-09-14 15:31:23 +0300

    updated  Advanced Features: Tagging and Search Functionality

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -3,6 +3,9 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm 
 from .models import Post, Tag
 from .models import Comment
+from taggit.forms import TagField
+from taggit.models import Tag
+from .widgets import TagWidget  # Import the custom TagWidget
 
   #user registration form
 class UserRegisterForm(UserCreationForm):
@@ -26,12 +29,19 @@ class UserUpdateForm(forms.ModelForm):
 class PostForm(forms.ModelForm):
     tags = forms.ModelMultipleChoiceField(
         queryset=Tag.objects.all(),
-        widget=forms.CheckboxSelectMultiple,
-        required = False
+        widget=TagWidget(),  # Use the custom TagWidget
+        required=False
     )
     class Meta:
         model = Post
         fields = ['title', 'content', 'tags']
+
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+        if commit:
+            instance.save()
+            self.save_m2m()  # Save the tags
+        return instance
 
 class CommentForm(forms.ModelForm):
     class Meta:
